65c870e1d37fa1e36db607e9795895462f117126
H-chandan <harichandanmohanty@gmail.com>
2021-09-20 23:05:44 +0530

    master - shield invalid inps & design imprvmnts

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,11 @@
-function doit() {
-    let celsius = document.getElementById('celsius').value;
-    let fahrenheit = 9 * (celsius / 5) + 32;
-    document.getElementById('output').innerHTML = fahrenheit;
-}
+function celsiusToFahrenheit() {
+    const celsius = document.getElementById('celsius').value;
+    console.log(isNaN(celsius));
+    if (isNaN(celsius) || celsius == '') {
+        alert("PLEASE ENTER A NUMBER");
+        return false;
+    }
+    const fahrenheit = 9 * (celsius / 5) + 32;
+    document.getElementById('output').innerHTML = fahrenheit.toFixed(2);
+    return true;
+};
